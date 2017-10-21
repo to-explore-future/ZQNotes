e5ed76ce7c1137de80dd4aa2623796abc3c3ba61
to-explore-future <869518570@qq.com>
2017-10-21 17:59:13 +0800

    简易的登录界面

---
+++ codes/ios/用户登陆界面/用户登陆界面/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  用户登陆界面
+//
+//  Created by to-explore-future on 17/10/21.
+//  Copyright © 2017年 to-explore-future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
