570c3f93e5534cd7459e8f691ac9bc87e9efc80a
to-explore-future <869518570@qq.com>
2017-11-25 08:40:28 +0800

    autolayout

---
+++ codes/ios/必须掌握 autolayout/必须掌握 autolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  必须掌握 autolayout
+//
+//  Created by to-explore-future on 17/11/24.
+//  Copyright © 2017年 to-explore-future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
