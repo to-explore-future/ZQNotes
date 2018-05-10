ad5b706e7f094dd6dfc9409e9c6bd699026e21f5
张强 <869518570@qq.com>
2018-05-10 11:14:54 +0800

    alot
    1.UUID 测试
    2.jcCloud 上传

---
+++ codes/ios/UUIDTest/UUIDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUIDTest
+//
+//  Created by sharingmobile on 2018/5/10.
+//  Copyright © 2018年 869518570@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ codes/ios/UUIDTest/UUIDTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UUIDTest
+//
+//  Created by sharingmobile on 2018/5/10.
+//  Copyright © 2018年 869518570@qq.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
